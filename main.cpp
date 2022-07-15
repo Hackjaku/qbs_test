7b23370dbc6a7b546383406c6443be62f8e143d4
Hackjaku <stressfest@rocketmail.com>
2022-07-15 09:29:18 +0200

    UPDATE: working directories

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include <QtWidgets/QMessageBox>
 
 int main(int argc, char *argv[]) {
+
     QApplication app(argc, argv);
-    QWidget window;
-    window.resize(320, 240);
-    window.setWindowTitle(
+    QWidget window1;
+    window1.resize(320, 240);
+    window1.setWindowTitle(
         QApplication::translate("toplevel", "Top-level widget")
     );
-    window.show();
+    window1.show();
     return app.exec();
 }
